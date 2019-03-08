c9b7b80a18dd3ad0a4b879e635ed576df94d1dda
JoshuaZero <lsh02040003@sina.cn>
2019-03-08 13:06:51 +0800

    add recurs

--- Algorithm/Algorithm.py
+++ Algorithm/Algorithm.py
@@ -32,5 +32,25 @@ def selectionSort(arr):    #对数组进行排序
         newArr.append(arr.pop(smallest))
     return newArr               #return 要空一个tab位置
 
+
+#recursion and while
+def look_for_key(main_box):
+    pile = main_box.make_a_pile_to_look_through()
+    while pile is not empty:
+        box = pile.grab_a_box()
+        for item in box:
+            if item.is_a_box():
+                pile.append(item)
+            elif item.is_a_key():
+                print("found the key!")
+
+
+def look_for_ley_B(box):
+    for item in box:
+        if item.is_a_box():
+            look_for_ley()      #递归
+        elif item.is_a_key():
+            print("found the key!")
+
 my_list = [1,3,4,5,6,9]
 print (binary_search(my_list,9))
