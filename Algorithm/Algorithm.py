89f6fe9f0fc1d5030a0fd11f2f15a01c64f3b491
JoshuaZero <lsh02040003@sina.cn>
2019-03-30 14:06:11 +0800

    test the minist

--- Algorithm/Algorithm.py
+++ Algorithm/Algorithm.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import tensorflow as tf
 
 def binary_search(list, item):
     low = 0                 #确定查找的范围
@@ -48,9 +49,31 @@ def look_for_key(main_box):
 def look_for_ley_B(box):
     for item in box:
         if item.is_a_box():
-            look_for_ley()      #递归
+            look_for_ley_B()      #递归
         elif item.is_a_key():
             print("found the key!")
+
+# quick sort algorithm
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+    print(quicksort([10, 5, 2, 3]))
+
+voted={}
+def check_voter(name):
+    if voted.get(name):
+        print("kick them out!")
+    else:
+        voted[name] = True
+        print("let them vote!")
+
+
+
 
 my_list = [1,3,4,5,6,9]
 print (binary_search(my_list,9))
